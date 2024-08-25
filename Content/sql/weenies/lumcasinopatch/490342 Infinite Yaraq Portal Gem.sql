d0c0d57287cbc285732f334d587dfd19646ad240
ZaiusAC <115657876+ZaiusAC@users.noreply.github.com>
2024-08-25 11:25:01 -0400

    fix

--- Content/sql/weenies/lumcasinopatch/490342 Infinite Yaraq Portal Gem.sql
+++ Content/sql/weenies/lumcasinopatch/490342 Infinite Yaraq Portal Gem.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 490342;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (490342, 'gemportalyaraq', 38, '2021-11-01 00:00:00') /* Gem */;
+VALUES (490342, 'gemportalyaraqinfinite', 38, '2021-11-01 00:00:00') /* Gem */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (490342,   1,       2048) /* ItemType - Gem */
