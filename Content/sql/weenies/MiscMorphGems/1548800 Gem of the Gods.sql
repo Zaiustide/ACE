e52fa9349e49c82370b2a7135c8a50a1d114c5ba
ZaiusAC <115657876+ZaiusAC@users.noreply.github.com>
2024-10-20 12:26:40 -0400

    Morph Gems - Content

--- Content/sql/weenies/MiscMorphGems/1548800 Gem of the Gods.sql
+++ Content/sql/weenies/MiscMorphGems/1548800 Gem of the Gods.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 1548800;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (1548800, 'ace1548800-heroicmastermorphgem', 38, '2022-01-29 01:15:03') /* Gem */;
+VALUES (1548800, 'ace1548800-gemofthegodsmorphgem', 38, '2022-01-29 01:15:03') /* Gem */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (1548800,   1,       2048) /* ItemType - Gem */
@@ -11,7 +11,7 @@ VALUES (1548800,   1,       2048) /* ItemType - Gem */
      , (1548800,  19,         25) /* Value */
      , (1548800,  65,        101) /* Placement - Resting */
      , (1548800,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (1548800,  94,      35215) /* TargetType - Vestements */;
+     , (1548800,  94,          8) /* TargetType - Jewelry */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (1548800,   1, False) /* Stuck */
@@ -23,27 +23,14 @@ VALUES (1548800,   1, False) /* Stuck */
      , (1548800,  69, False) /* IsSellable */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (1548800,   1, 'Heroic Master Morph Gem') /* Name */
-     , (1548800,  14, 'Applying this gem to loot type items has a chance to alter its Workmanship.') /* Use */;
+VALUES (1548800,   1, 'Gem of the Gods') /* Name */
+     , (1548800,  14, 'Applying this gem to loot type jewelry has a chance to apply one to three spells that can be found on Gauntlet Jewelry, Rare Armor and Rare Weapons. Can only be applied once per piece of jewelry.') /* Use */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES  (1548800,   1, 0x02000179) /* Setup */
+VALUES (1548800,   1, 0x02000179) /* Setup */
      , (1548800,   3, 0x20000014) /* SoundTable */
      , (1548800,   6, 0x04000BEF) /* PaletteBase */
      , (1548800,   7, 0x1000010B) /* ClothingBase */
      , (1548800,  22, 0x3400002B) /* PhysicsEffectTable */
-     , (1548800,  8, 0x060026BA) /* Icon */
-     , (1548800,  52, 0x060065FB) /* IconUnderlay */;
-
-/* Lifestoned Changelog:
-{
-  "Changelog": [
-    {
-      "created": "2022-01-17T02:18:55.5489445Z",
-      "author": "ACE.Adapter",
-      "comment": "Weenie exported from ACEmulator world database using ACE.Adapter"
-    }
-  ],
-  "IsDone": false
-}
-*/
+     , (1548800,  8, 0x06006816) /* Icon */
+     , (1548800,  52, 0x06005B0C) /* IconUnderlay */;
--- Content/sql/weenies/MiscMorphGems/1548801 Potion of Resistance.sql
+++ Content/sql/weenies/MiscMorphGems/1548801 Potion of Resistance.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 1548801;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (1548801, 'ace1548801-netherresistmorphgem', 38, '2022-01-29 01:15:03') /* Gem */;
+VALUES (1548801, 'ace1548801-potionofresistancemorphgem', 38, '2022-01-29 01:15:03') /* Gem */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (1548801,   1,       2048) /* ItemType - Gem */
@@ -11,7 +11,7 @@ VALUES (1548801,   1,       2048) /* ItemType - Gem */
      , (1548801,  19,         25) /* Value */
      , (1548801,  65,        101) /* Placement - Resting */
      , (1548801,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (1548801,  94,      35215) /* TargetType - Vestements */;
+     , (1548801,  94,          8) /* TargetType - Jewelry */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (1548801,   1, False) /* Stuck */
@@ -23,27 +23,14 @@ VALUES (1548801,   1, False) /* Stuck */
      , (1548801,  69, False) /* IsSellable */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (1548801,   1, 'Nether Resist Morph Gem') /* Name */
-     , (1548801,  14, 'Applying this gem to loot type items has a chance to alter its Workmanship.') /* Use */;
+VALUES (1548801,   1, 'Potion of Resistance') /* Name */
+     , (1548801,  14, 'Applying this potion to a trinket adds additional protection against damage over time spells. Applying this to your trinket will add Sigil of Purity to the trinket. This is random between level II and VIII, it does not stack with growth aetheria. Can be applied to a trinket once.') /* Use */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES  (1548801,   1, 0x02000179) /* Setup */
+VALUES (1548801,   1, 0x02000179) /* Setup */
      , (1548801,   3, 0x20000014) /* SoundTable */
      , (1548801,   6, 0x04000BEF) /* PaletteBase */
      , (1548801,   7, 0x1000010B) /* ClothingBase */
      , (1548801,  22, 0x3400002B) /* PhysicsEffectTable */
-     , (1548801,  8, 0x060026BA) /* Icon */
-     , (1548801,  52, 0x060065FB) /* IconUnderlay */;
-
-/* Lifestoned Changelog:
-{
-  "Changelog": [
-    {
-      "created": "2022-01-17T02:18:55.5489445Z",
-      "author": "ACE.Adapter",
-      "comment": "Weenie exported from ACEmulator world database using ACE.Adapter"
-    }
-  ],
-  "IsDone": false
-}
-*/
+     , (1548801,  8, 0x060066B7) /* Icon */
+     , (1548801,  52, 0x06005B0C) /* IconUnderlay */;
