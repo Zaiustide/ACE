f6e5a77a600cf0ae52855e615c7b0a207aaf8e27
ZaiusAC <115657876+ZaiusAC@users.noreply.github.com>
2025-03-15 13:36:07 -0400

    fix

--- Content/sql/weenies/Arena Patch/490496 Boxed Imbue Swap Coin.sql
+++ Content/sql/weenies/Arena Patch/490496 Boxed Imbue Swap Coin.sql
@@ -1,38 +1,38 @@
-DELETE FROM `weenie` WHERE `class_Id` = 490296;
+DELETE FROM `weenie` WHERE `class_Id` = 490496;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (490296, 'ace490296-Boxed Imbue Swap Coin', 38, '2021-11-01 00:00:00') /* Gem */;
+VALUES (490496, 'ace490496-Boxed Imbue Swap Coin', 38, '2021-11-01 00:00:00') /* Gem */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
-VALUES (490296,   1,        128) /* ItemType - Misc */
-     , (490296,   5,         50) /* EncumbranceVal */
-     , (490296,  11,          1) /* MaxStackSize */
-     , (490296,  12,          1) /* StackSize */
-     , (490296,  13,         50) /* StackUnitEncumbrance */
-     , (490296,  15,          5) /* StackUnitValue */
-     , (490296,  16,          8) /* ItemUseable - Contained */
-     , (490296,  19,          5) /* Value */
-     , (490296,  33,          1) /* Bonded - Bonded */
-     , (490296,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
-     , (490296,  94,         16) /* TargetType - Creature */
-     , (490296, 114,          1) /* Attuned - Attuned */
-     , (490296, 269,          1) /* UseCreateQuantity */;
+VALUES (490496,   1,        128) /* ItemType - Misc */
+     , (490496,   5,         50) /* EncumbranceVal */
+     , (490496,  11,          1) /* MaxStackSize */
+     , (490496,  12,          1) /* StackSize */
+     , (490496,  13,         50) /* StackUnitEncumbrance */
+     , (490496,  15,          5) /* StackUnitValue */
+     , (490496,  16,          8) /* ItemUseable - Contained */
+     , (490496,  19,          5) /* Value */
+     , (490496,  33,          1) /* Bonded - Bonded */
+     , (490496,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (490496,  94,         16) /* TargetType - Creature */
+     , (490496, 114,          1) /* Attuned - Attuned */
+     , (490496, 269,          1) /* UseCreateQuantity */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
-VALUES (490296,  69, False) /* IsSellable */;
+VALUES (490496,  69, False) /* IsSellable */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (490296,  39,     0.5) /* DefaultScale */;
+VALUES (490496,  39,     0.5) /* DefaultScale */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
-VALUES (490296,   1, 'Boxed Imbue Swap Coin') /* Name */
-     , (490296,  14, 'Use this crate to retrieve its contents.') /* Use */
-     , (490296,  16, 'A box containing one Imbue Swap Coin.') /* LongDesc */;
+VALUES (490496,   1, 'Boxed Imbue Swap Coin') /* Name */
+     , (490496,  14, 'Use this crate to retrieve its contents.') /* Use */
+     , (490496,  16, 'A box containing one Imbue Swap Coin.') /* LongDesc */;
 
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES (490296,   1, 0x0200011E) /* Setup */
-     , (490296,   3, 0x20000014) /* SoundTable */
-     , (490296,   8, 0x06006635) /* Icon */
-	 , (490296,  50, 0x06006910) /* IconOverlay */
-     , (490296,  22, 0x3400002B) /* PhysicsEffectTable */
-     , (490296,  38,      45494) /* UseCreateItem - Blank Augmentation Gem */;
+VALUES (490496,   1, 0x0200011E) /* Setup */
+     , (490496,   3, 0x20000014) /* SoundTable */
+     , (490496,   8, 0x06006635) /* Icon */
+	 , (490496,  50, 0x06006910) /* IconOverlay */
+     , (490496,  22, 0x3400002B) /* PhysicsEffectTable */
+     , (490496,  38,      45494) /* UseCreateItem - Blank Augmentation Gem */;
