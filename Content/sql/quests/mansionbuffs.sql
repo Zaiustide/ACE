3f9ea03c8ce97ce01e3b5d11041027731c33e137
ZaiusAC <115657876+ZaiusAC@users.noreply.github.com>
2025-11-23 11:52:57 -0500

    fixes

--- Content/sql/quests/mansionbuffs.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `quest` WHERE `name` = 'mansionbuffs';
-
-INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
-VALUES ('mansionbuffs', 20, -1, 'cooldown for virindi servant mansion buffs', '2021-11-01 00:00:00');
