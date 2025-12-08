8c74e7fac54c6eace8fd6fbd9f7cd40cafcdf6e7
AcPvP <89650671+AcPvP@users.noreply.github.com>
2025-12-08 18:38:41 -0500

    Oops

--- Content/sql/weenies/T9Dungeons/451610941 Torgrym the Magnificent.sql
+++ Content/sql/weenies/T9Dungeons/451610941 Torgrym the Magnificent.sql
@@ -154,7 +154,6 @@ VALUES (451610941, 9, 1548801,  1, 0,  0.02, False) /* 1548801 Potion of Resista
 	 , (451610941, 9, 490273,  1, 0,   0.05, False) /* 490273 Rare Gem of Critical Damage Reduction */
 	 , (451610941, 9, 490272,  1, 0,   0.05, False) /* 490272 Rare Gem of Critical Damage */ 
 	 , (451610941, 9,      0,  0, 0,    0.9, False)    /* Create nothing for ContainTreasure */
-	 , (451610941, 9, 41541201,  1, 0, 0.15, False) /* 41541201 Paragon Legendary Quest Gem */ 
 	 , (451610941, 9, 41541202,  1, 0, 0.15, False) /* 41541202 Paragon Legendary Quest Gem */ 
 	 , (451610941, 9,      0,  0, 0,   0.85, False)    /* Create nothing for ContainTreasure */
 	 , (451610941, 9,   600058,  1, 0, 0.2, False) /* 600058 Oil of the Elements Gem */ 
