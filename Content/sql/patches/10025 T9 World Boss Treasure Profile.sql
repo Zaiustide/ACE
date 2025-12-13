18a47c73e6a9effe3c34f002abbf24f6c2154c82
AcPvP <89650671+AcPvP@users.noreply.github.com>
2025-12-13 15:01:01 -0500

    World Bosses T9 loot profile

---
+++ Content/sql/patches/10025 T9 World Boss Treasure Profile.sql
@@ -0,0 +1,21 @@
+DELETE FROM ace_world.treasure_death WHERE treasure_Type = 10025;
+
+INSERT INTO ace_world.treasure_death
+SET
+  treasure_Type = 10025,
+  tier = 9,
+  loot_Quality_Mod = 0.8125,
+  unknown_Chances = 19,
+  item_Chance = 1,
+  item_Min_Amount = 0,
+  item_Max_Amount = 1,
+  item_Treasure_Type_Selection_Chances = 12,
+  magic_Item_Chance = 100,
+  magic_Item_Min_Amount = 10,
+  magic_Item_Max_Amount = 20,
+  magic_Item_Treasure_Type_Selection_Chances = 22,
+  mundane_Item_Chance = 1,
+  mundane_Item_Min_Amount = 0,
+  mundane_Item_Max_Amount = 1,
+  mundane_Item_Type_Selection_Chances = 9
+;
